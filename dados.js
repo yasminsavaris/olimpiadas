af3494c177ef15dd9d8b1da59bd4cf3900015a44
Yasmin Savaris <87158763+yasminsavaris@users.noreply.github.com>
2024-09-07 09:04:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas competições internacionais. Com técnica apurada e determinação, ela conquistou diversas medalhas e se tornou um dos principais nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "judo judô judoca judoka"
+
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade, a jovem ginasta brasileira, conquistou o coração dos brasileiros com suas performances excepcionais. Com talento inegável e muita dedicação, ela se tornou um dos maiores nomes da ginástica artística mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginastica ginástica ouro"
+
+    }
+];
